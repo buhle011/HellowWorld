bafb22e05b396366fd73180ee5b5523b17285502
lungani delihlazo <banzi5380@gmail.com>
2025-02-06 14:22:53 +0200

    Java code

---
+++ Hellocode.java
@@ -0,0 +1,13 @@
+package HelloCode;
+
+public class Hellocode {
+	
+	 public static void main (String[]args){
+
+		   for(int i =1;i<=5;i++){
+
+		       System.out.println("Hello World... "+i);
+
+		   }
+		 }
+}
